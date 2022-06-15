7c15fe0dcf623b1c477a0e625e0c51d4da88b1a0
luizfernando_lf@hotmail.com.br <luizfernando_lf@hotmail.com.br>
2022-06-15 08:59:03 -0300

    chore: add exercise 10

---
+++ questao-10/main.js
@@ -0,0 +1,11 @@
+function isChocolate() {
+  let iceCream = "chocolate";
+  
+  if (iceCream === "chocolate") {
+    alert("Amo sorvete de chocolate");
+    return;
+  }
+  
+  alert("Prefiro outros sabores");
+  return;
+}
